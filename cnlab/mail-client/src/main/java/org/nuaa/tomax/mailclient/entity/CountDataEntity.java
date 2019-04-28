bd43849020b743ff55d8932f50fa58d146e07a1c
tomax <1121584497@qq.com>
2019-04-28 17:43:30 +0800

    complete demo

---
+++ cnlab/mail-client/src/main/java/org/nuaa/tomax/mailclient/entity/CountDataEntity.java
@@ -0,0 +1,11 @@
+package org.nuaa.tomax.mailclient.entity;
+
+/**
+ * @Name: CountDataEntity
+ * @Description: TODO
+ * @Author: tomax
+ * @Date: 2019-04-27 10:59
+ * @Version: 1.0
+ */
+public class CountDataEntity {
+}

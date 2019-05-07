17c8164dc44930246d56f1a70563cb9fa4c5705c
XingToMax <1121584497@qq.com>
2019-05-07 23:00:24 +0800

    add im server

---
+++ cnlab/im-server/src/main/java/org/nuaa/tomax/imserver/ImServerApplication.java
@@ -0,0 +1,13 @@
+package org.nuaa.tomax.imserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImServerApplication.class, args);
+	}
+
+}

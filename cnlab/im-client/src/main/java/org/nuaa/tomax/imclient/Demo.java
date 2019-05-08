6caa1def403a995f9f1a231443d6ba16bf06c3ef
XingToMax <1121584497@qq.com>
2019-05-08 23:42:51 +0800

    init im client

---
+++ cnlab/im-client/src/main/java/org/nuaa/tomax/imclient/Demo.java
@@ -0,0 +1,9 @@
+package org.nuaa.tomax.imclient;
+
+/**
+ * @Author: ToMax
+ * @Description:
+ * @Date: Created in 2019/5/8 23:41
+ */
+public class Demo {
+}
---
+++ cnlab/im-client/src/main/java/org/nuaa/tomax/imclient/core/Message.java
@@ -0,0 +1,9 @@
+package org.nuaa.tomax.imclient.core;
+
+/**
+ * @Author: ToMax
+ * @Description:
+ * @Date: Created in 2019/5/8 23:42
+ */
+public class Message {
+}
